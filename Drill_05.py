f8cadf102a2555bcd6c6105f69d395aa2a56425e
SWnotebook <sungwoo5957@naver.com>
2023-10-04 22:53:05 +0900

    Drill_05 에서도 frame 초기화 처리. action 변수 처리 방식 변경.

--- Drill_05.py
+++ Drill_05.py
@@ -27,10 +27,9 @@ def handle_events():
 
 def reset_world():
     global running, cx, cy, frame
-    global hx, hy
-    global sx, sy
     global t
     global action
+
     running = True
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
@@ -41,10 +40,14 @@ def reset_world():
 
 def set_new_target_arrow():
     global sx, sy, hx, hy, t
+    global action
+    global frame
     sx, sy = cx, cy  # p1 : 시작점
     hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2 : 끝점
     # hx,hy = 100 , 100
     t = 0.0
+    action = 1 if sx < hx else 0
+    frame = 0
 
 
 def render_world():
@@ -59,10 +62,8 @@ def update_world():
     global frame
     global cx, cy
     global t
-    global action
 
     frame = (frame + 1) % 8
-    action = 1 if cx < hx else 0
 
     if t <= 1.0:
         cx = (1 - t) * sx + t * hx  # cx는 시작x 끝x를 1-t:t로 섞은 위치
